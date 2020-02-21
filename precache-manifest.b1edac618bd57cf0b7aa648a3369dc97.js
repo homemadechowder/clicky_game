0afb156849fc71e0fdc5d9b588b15b806541720b
Andrew Lu <awclu@ucdavis.edu>
2020-02-20 21:45:04 -0800

    Updates

---
+++ precache-manifest.b1edac618bd57cf0b7aa648a3369dc97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee66852843a6492f9cb7f84b9b893c1",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "d276a50367b4ef90d299",
+    "url": "/clicky_game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ab30a9dc41bb992ce602",
+    "url": "/clicky_game/static/js/2.51fbfe4a.chunk.js"
+  },
+  {
+    "revision": "27df78d2822439c2fc110c0a385025d0",
+    "url": "/clicky_game/static/js/2.51fbfe4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d276a50367b4ef90d299",
+    "url": "/clicky_game/static/js/main.efbf4b7f.chunk.js"
+  },
+  {
+    "revision": "1ac68db4dcbf3939a67e",
+    "url": "/clicky_game/static/js/runtime-main.454c3990.js"
+  }
+]);
